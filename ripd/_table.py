d71e0c6d6ff43d93c6220bf73335a2c46634328c
KahuJones <kjo110@uclive.ac.nz>
2025-04-22 13:48:35 +1200

    Flake8 Checks completed

--- ripd/_table.py
+++ ripd/_table.py
@@ -5,14 +5,13 @@ import time
 from _structures import RipEntry, RipPacket
 
 
-
-
 class RouteEntry:
     """
         Represents a single entry in the routing table.
         Stores the destination address, next hop, metric, and timeout.
     """
-    def __init__(self, destination, next_hop, metric, timeout, garbage_collect=None):
+    def __init__(self, destination, next_hop,
+                 metric, timeout, garbage_collect=None):
         self.destination = destination
         self.next_hop = next_hop
         self.metric = metric
@@ -23,12 +22,13 @@ class RouteEntry:
         """
             Convert the RouteEntry to a RipEntry for packet transmission.
         """
-        
         return RipEntry(
             addr=self.destination,
-            next_hop=self.next_hop, # Needs to be added to RipEntry - Assumed it would be neeeded. 
+            # Needs to be added to RipEntry
+            next_hop=self.next_hop,
             metric=self.metric
         )
+
 
 class RouteTable:
     """
@@ -42,7 +42,8 @@ class RouteTable:
     def add_route(self, destination, next_hop, metric):
         """
             Add a new route to the routing table.
-            If the route already exists, update it with the new metric and timeout.
+            If the route already exists,
+            update it with the new metric and timeout.
         """
         timeout = time.time() + 180  
         entry = RouteEntry(destination, next_hop, metric, timeout)
@@ -54,7 +55,10 @@ class RouteTable:
             If the route does not exist, do nothing.
         """
         # Find the entry with the matching destination and remove it
-        self.routes = [entry for entry in self.routes if entry.destination != destination]
+        self.routes = [
+            entry for entry in self.routes
+            if entry.destination != destination
+            ]
 
     def remove_all(self):
         """
